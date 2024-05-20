602a0fba5304b200df056f0c1c7f4957927e88ef
Martin Meredith <martin@sourceguru.net>
2024-05-20 21:12:54 +0100

    Respond with 200

--- app/server.go
+++ app/server.go
@@ -2,20 +2,74 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"os"
+	"net/http"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	log.Println("Starting server on port 4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		log.Panicln("Failed to bind to port 4221")
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			log.Panicln("Failed to close listener")
+		}
+	}(listener)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Panicln("Error accepting connection: ", err.Error())
+		}
+
+		go handleConnection(conn)
 	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+
+	read, err := conn.Read(buffer)
+	if err != nil {
+		log.Panicln("Error reading: ", err.Error())
+	}
+
+	buffer = buffer[:read]
+	log.Println("Received: ", string(buffer))
+
+	writeResponse(conn, http.StatusOK, []string{"Content-Length: 5", "Content-Type: text/plain"}, "Hello")
+}
+
+func writeLine(conn net.Conn, data string) {
+	_, err := conn.Write([]byte(data + "\r\n"))
+	if err != nil {
+		log.Panicln("Error writing: ", err.Error())
+	}
+}
+
+func writeHeaders(conn net.Conn, headers []string) {
+	for _, header := range headers {
+		writeLine(conn, header)
+	}
+}
+
+func getHttpResponseLine(status int) string {
+	return fmt.Sprintf("HTTP/1.1 %d %s", status, http.StatusText(status))
+}
+
+func writeStatusLine(conn net.Conn, status int) {
+	writeLine(conn, getHttpResponseLine(status))
+}
+
+func writeResponse(conn net.Conn, status int, headers []string, body string) {
+	writeStatusLine(conn, status)
+	writeHeaders(conn, headers)
+	writeLine(conn, "")
+	writeLine(conn, body)
 }
